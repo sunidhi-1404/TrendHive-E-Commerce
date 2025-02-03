d93f061fa40256c4456e35b43a6846153c130f4e
Sunidhi Chauhan <102759690+sunidhi-1404@users.noreply.github.com>
2025-02-03 21:33:38 +0530

    first1

---
+++ admin/src/main.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// // import { StrictMode } from 'react'
+// import { ReactDOM } from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+// import ShopContextProvider from "./context/ShopContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
